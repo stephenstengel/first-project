c37610e70650368ebdfad68d77c3a278f8cb085e
Stephen <stephen.stengel@cwu.edu>
2021-12-22 21:55:06 -0800

    Added a button that changes the background color. Uses depricated function.

--- hello-world.py
+++ hello-world.py
@@ -1,17 +1,23 @@
 # ~ MIT License
 # ~ Copyright (c) <2021> <stephen.stengel@cwu.edu>
 
+#This is a test program using glade and gtk through pygobject
+
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk ##Gdk is needed to change background color?
 from os import system
 from os import kill
+from time import sleep
+import random
 
 import subprocess
 import signal
 
 class Handler:
 	myProcess = None
+	currentBrightness = 1.0
+	flashCount = 0
 	
 	def button1_clicked(self, button):
 		print("Start music button pressed!")
@@ -25,7 +31,21 @@ class Handler:
 			print("Killing this process: " + str(self.myProcess.pid))
 			kill(self.myProcess.pid, signal.SIGKILL)
 			self.myProcess = None
+	
+	def rave_button_clicked_cb(self, button):
+		print("Rave button clicked!")
+		r, g, b = random.random(), random.random(), random.random()
+		if self.currentBrightness == 1.0:
+			self.currentBrightness = 0.5
+		else:
+			self.currentBrightness = 1.0
 
+		##window just hangin there is not great. depends on script.
+		##I think I saw a tutorial that used a class to contain the window setup.
+		window.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(r, g, b, self.currentBrightness)) #depricated call
+		print("currentBrightness: " + str(self.currentBrightness))
+		
+		
 builder = Gtk.Builder()
 builder.add_from_file("glade-button.glade")
 builder.connect_signals(Handler())
@@ -36,7 +56,21 @@ start_button.set_label("Play the music!")
 stop_button = builder.get_object("music_stop")
 stop_button.set_label("Stop the music!")
 
+rave_button = builder.get_object("rave_button")
+rave_button.set_label("Change color!")
+
+# ~ draw_area = builder.get_object("draw_area")
+vol_slider = builder.get_object("volume_slider")
+# ~ vol_slider.set_label("Volume! It isn't connected to anything yet.")
+
+label1 = builder.get_object("label1")
+# ~ label1.set_label("Test label!")
+
+myViewport = builder.get_object("myViewport")
+
 window = builder.get_object("myWindow")
+# ~ window.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(0, 0, 0, 1)) #depricated call
+
 
 window.connect("destroy", Gtk.main_quit)
 window.show_all()
