59bcfd7cbca3ddee3f6b8f11269859685f75ec4c
Stephen <stephen.stengel@cwu.edu>
2022-02-13 04:08:49 -0800

    Another working version with the MyWindow class in a separate file.

--- hello-world.py
+++ hello-world.py
@@ -18,38 +18,7 @@ import random
 import subprocess
 import signal
 
-from Handlers import Handlers
-
-class MyWindow(Gtk.Window):
-	def __init__(self):
-		super().__init__(title = "RAVE TIME!")
-
-		builder = Gtk.Builder()
-		builder.add_from_file("glade-button.glade")
-		
-		#I'm going to make a list of all the objects that Handlers()
-		#needs to know about.
-		jigglypuff = builder.get_object("Jigglypuff")
-		
-		start_button = builder.get_object("music_start")
-		start_button.set_label("Play the music!")
-		
-		stop_button = builder.get_object("music_stop")
-		stop_button.set_label("Stop the music!")
-		stop_button.set_sensitive(False)
-		
-		rave_button = builder.get_object("rave_button")
-		rave_button.set_label("Change color!")
-		
-		label1 = builder.get_object("label1")
-		
-		window = builder.get_object("myWindow")
-		window.connect("destroy", Gtk.main_quit)
-		
-		myHandlers = Handlers(jigglypuff, stop_button, label1, window)
-		builder.connect_signals(myHandlers)
-		
-		window.show_all()
+from MyWindow import MyWindow
 
 
 
